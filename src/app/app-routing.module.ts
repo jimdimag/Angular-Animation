ace22b6da2261d8df0fd2dfbe1343af548b3b22b
Jim DiMaggio <jimdimag@gmail.com>
2019-06-13 08:20:23 -0400

    added routing to project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,53 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BasicComponent } from './basic/basic.component';
+import { AdvancedComponent } from './advanced/advanced.component';
+import { ContainedComponent } from './contained/contained.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+
+    path: 'animation-home',
+
+    component: HomeComponent
+
+  },
+
+  {
+
+    path: 'animation-basics',
+
+    component: BasicComponent
+
+  },
+
+  {
+
+    path: 'animation-contained',
+
+    component: ContainedComponent
+
+  },
+
+  {
+
+    path: 'animation-advanced',
+
+    component: AdvancedComponent
+
+  },
+
+  {
+
+    path: '',
+
+    redirectTo: '/animation-home',
+
+    pathMatch: 'full'
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BasicComponent } from './basic/basic.component';
+import { AdvancedComponent } from './advanced/advanced.component';
+import { ContainedComponent } from './contained/contained.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BasicComponent,
+    AdvancedComponent,
+    ContainedComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
